4e3de7dbb78e5619c43c0f3012af90ffc0bd9d3d
Alejandro Verri <averri@gmail.com>
2024-07-06 12:47:24 -0300

    fix map_OCID

--- R/map_OCID.R
+++ R/map_OCID.R
@@ -14,7 +14,7 @@ map_OCID <- function(.x){
   DT <- copy(.x)
   stopifnot(names(DT) %in% c("OCID","AT"))
   UP_ID <- c("BHZ","HHZ","HLZ","HNZ","HN3","HGZ","DWN","DN","Z","DOWN","V","VER","VERT","VERTICAL","VRT","UD","UP","UPDO") |> unique()
-  OD <- DT[,.(PGA=max(abs(AT)),isUP=(OCID %in% UP_ID),DIR=""),by=.(OCID)]
+  OD <- DT[,.(PGA=max(abs(AT)),isUP=(toupper(OCID) %in% UP_ID),DIR=""),by=.(OCID)]
   OD[isUP==TRUE,DIR:="UP"]
   OD[isUP==FALSE, DIR := ifelse(PGA == max(PGA), "H1", "H2")]
   OD[,`:=`(isUP=NULL,PGA=NULL)]
