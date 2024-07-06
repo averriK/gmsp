c392ebe71cc94908405d967adb77d30bfaf493d0
Alejandro Verri <averri@gmail.com>
2024-07-06 18:32:22 -0300

    get_ND

--- R/get_Newmark.R
+++ R/get_Newmark.R
@@ -3,6 +3,7 @@
 #' @param ATL data.table
 #' @param TargetUnits character
 #' @param kh_values vector
+#' @param FULL boolean
 #'
 #' @return
 #' @export
@@ -11,7 +12,7 @@
 
 
 
-get_Newmark <- function(ATL,TargetUnits="mm",kh_values=c(0.01,0.02,0.05, 0.10, 0.15, 0.20, 0.25, 0.30,0.35, 0.40,0.45, 0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.90,1.00,1.25,1.50,1.75,2.00,2.50,3.00)){
+get_Newmark <- function(ATL,TargetUnits="mm",kh_values=c(0.01,0.02,0.05, 0.10, 0.15, 0.20, 0.25, 0.30,0.35, 0.40,0.45, 0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.90,1.00,1.25,1.50,1.75,2.00,2.50,3.00,FULL=FALSE)){
   on.exit(expr={rm(list = ls())}, add = TRUE)
   . <- NULL
   g <- .getG(TargetUnits) #GMSP$g
@@ -21,7 +22,7 @@ get_Newmark <- function(ATL,TargetUnits="mm",kh_values=c(0.01,0.02,0.05, 0.10, 0
     sapply(kh_values, function(kh) {
       ATL[, .(
         ID = sprintf("DN%02d", round(kh * 100)), 
-        value = .get_ND(AT = s, t = t, kh = kh, FULL = FALSE)), 
+        value = get_ND(AT = s, t = t, kh = kh, FULL = FALSE)), 
         by = .(RecordSN, DIR, OCID)]}, simplify = FALSE))
   NDW <- dcast(NDL, RecordSN + OCID + DIR ~ ID, value.var = "value")
   return(list(NDL=NDL,NDW=NDW))
